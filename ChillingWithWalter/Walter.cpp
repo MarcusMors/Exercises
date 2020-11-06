315eaca8c3351e09e367ca685235da101794ebce
Marcus Mors <alivezeh@gmail.com>
2020-11-05 22:25:41 -0500

    IS REACHABLE, working but return value doesn't and arr is always empty

--- ChillingWithWalter/Walter.cpp
+++ ChillingWithWalter/Walter.cpp
@@ -20,6 +20,13 @@ Input
 4 3 3
 Output
 3
+input
+3
+5 5 8
+6 1 5
+8 0 4
+Output
+9
 */
 int pass;
 
@@ -33,7 +40,6 @@ void printMx3(long long arr[][3], long long length)
 		}
 		std::cout << "\n";
 	}
-	std::cout << "\n";
 }
 
 void printMx2(long long arr[][2], long long length)
@@ -46,7 +52,6 @@ void printMx2(long long arr[][2], long long length)
 		}
 		std::cout << "\n";
 	}
-	std::cout << "\n";
 }
 void printArr(long long arr[], int length)
 {
@@ -57,13 +62,33 @@ void printArr(long long arr[], int length)
 	std::cout << std::endl;
 }
 
-long long abs(long long num)
+// long long abs(long long num)
+// {
+// 	return (num < 0) ? (num * -1) : num;
+// }
+
+bool reachable(long long start[2], long long end[2]) //angle of 45°
 {
-	return (num < 0) ? num * -1 : num;
-}
-bool reachable(long long pos1[2], long long pos2[2]) //angle of 45°
-{
-	return ((pos1[0] <= pos2[0]) && (pos1[1] <= pos2[1])) ? true : false;
+	// long long x = end[0] - start[0];
+	// long long y = abs((end[1] - start[1]));
+	// cout << "\nstart\t: ";
+	// printArr(start, 2);
+	// cout << "end\t: ";
+	// printArr(end, 2);
+	// cout << "x , y \t: " << x << "\t" << y << endl;
+
+	// if (x >= 0)
+	// {
+	// 	cout << x << " >= 0" << endl;
+	// 	if (x >= y)
+	// 	{
+	// 		cout << x << " >= " << y << endl;
+	// 		return true;
+	// 	}
+	// 	cout << x << " <= " << y << endl;
+	// }
+	// return false;
+	return ((x >= 0) && (x >= y)) ? true : false;
 }
 
 long long max(long long arr[], long long length)
@@ -86,23 +111,14 @@ long long walter(long long start[2], long long bearingsPoints[][3] = {0}, long l
 
 	if (length == 0)
 	{
-		cout << accumulated << endl;
+		cout << "cero exit\t: " << accumulated << endl;
 		return accumulated;
 	}
 	else if (length == 1)
 	{
-		// if (bearingsPoints[0][0] == 0)
-		// {
-		// 	cout << accumulated + bearingsPoints[0][3] << endl;
-		// 	return accumulated;
-		// }
-		cout << accumulated + bearingsPoints[0][2] << endl;
+		cout << "else exit\t: " << accumulated + bearingsPoints[0][2] << endl;
 		return accumulated + bearingsPoints[0][2];
 	}
-
-	cin >> pass;
-	accumulated += bearingsPoints[0][2];
-	cout << accumulated << endl;
 
 	long long arr[length];
 	long long auxStart[2];
@@ -111,8 +127,9 @@ long long walter(long long start[2], long long bearingsPoints[][3] = {0}, long l
 
 	for (long long i = 0; i < length; i++)
 	{
-		long long auxLength = (length - i) - 1;
+		long long auxLength = (length);
 		long long auxBearingsPoints[auxLength][3];
+		cout << "\n\t auxLength = " << auxLength << endl;
 
 		counter = 0;
 		auxStart[0] = bearingsPoints[i][0];
@@ -124,18 +141,27 @@ long long walter(long long start[2], long long bearingsPoints[][3] = {0}, long l
 			{
 				auxEnd[0] = bearingsPoints[j][0];
 				auxEnd[1] = bearingsPoints[j][1];
+
 				if (reachable(auxStart, auxEnd))
 				{
+					cout << "IS REACHABLE\t: " << counter << endl;
+					printArr(auxStart, 2);
+					printArr(auxEnd, 2);
 					auxBearingsPoints[counter][2] = bearingsPoints[j][2];
 					counter++;
 				}
 			}
 		}
-		arr[i] = walter(auxStart, auxBearingsPoints, counter, accumulated);
+		cin >> pass;
+		cout << accumulated << "\n\n";
+		arr[i] = walter(auxStart, auxBearingsPoints, counter, accumulated + bearingsPoints[i][2]);
+		cout << "arr[" << i << "]\t= " << arr[i] << endl;
 	}
+	cout << "arr:" << endl;
 	printArr(arr, counter);
 	cin >> pass;
-	return max(arr, counter);
+	// return max(arr, counter);
+	return accumulated;
 }
 
 int main()
@@ -164,6 +190,11 @@ int main()
 		bearingsPoints[count][2] = auxBearingsPoints[2];
 		count++;
 	}
+
+	printArr(start, 2);
+	cout << "length\t: " << count << endl;
+	printMx3(bearingsPoints, count);
+	cout << "SENT\n\n";
 
 	maxPoints = walter(start, bearingsPoints, count, 0);
 	std::cout << "MAX\t: " << maxPoints << std::endl;
